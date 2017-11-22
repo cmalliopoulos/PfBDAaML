b91e54283509cc53ed3bf6a425231d5a4e9d0dfc
Christos Malliopoulos <cmalliopoulos@gmail.com>
2017-11-22 19:54:19 +0200

    removed changeNBLayout.ipynb

--- changeNBLayout.py
+++ changeNBLayout.py
@@ -1,23 +1,28 @@
 import IPython.core.display as dis
 from IPython.core.display import HTML, display
 
+display(HTML('''
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+    </style> '''))
+
 # Change the default cell width for all cell types (code, raw, markdown, mathJax)
-display(HTML('<style> .container {width: 90% !important} </style>'))
+display(HTML('<style> .container {width: 90%} </style>'))
 
 display(HTML('''
     <style> .CodeMirror {font-size: 10.5pt !important} </style>'''))
 
 display(HTML('''
-    <style> div.cell.selected{border-left-width: 5px !important}</style>'''))
+    <style> div.cell.selected{border: 2px};</style>'''))
 
-# Change the focnt used by markdown cells
+# Change the font used by markdown cells
 # Change the line-spacing between text lines
 # Change the size of body text in markdown cells
 display(HTML('''<style> 
     .text_cell_render {
-        font-family: "roboto condensed"; 
+        font-family: "Roboto Condensed"; 
         line-height: 145%; 
-        font-size: 13pt} </style>'''))
+        font-size: 14pt} </style>'''))
 
 # Change the size of text in code cells
 # Change the size of text in code-cell output
